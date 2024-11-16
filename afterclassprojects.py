7c5fcc09606d35438e9f34ddb2169fdc72b21e3b
Alvin-558-png-w <alvin.nderui@gmail.com>
2024-11-16 18:49:51 +0300

    Initial aftclassact

---
+++ afterclassprojects.py
@@ -0,0 +1,16 @@
+
+x = 5
+y = 9
+z = 1
+
+print("Original values:")
+print("x =", x)
+print("y =", y)
+print("z =", z)
+
+x, y, z = z, y, x
+
+print("\nSwapped values:")
+print("x =", x)
+print("y =", y)
+print("z =", z)
